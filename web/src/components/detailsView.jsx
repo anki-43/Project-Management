f1e189cdb6389db4a54a91ae8c4a9de010809fd9
code-an-kit <ankitpathrikar@gmail.com>
2025-03-01 22:53:55 +0530

    add all chnages

--- web/src/components/detailsView.jsx
+++ web/src/components/detailsView.jsx
@@ -42,7 +42,7 @@ function DetailView() {
       </Box>
       <Box sx={{ display: "flex", gap: "20px", flex: 9, height: "90vh" }}>
         <LeftSideBar></LeftSideBar>
-        <Box className="section" sx={{ mt: "20px" }}>
+        <Box className="section">
           <Header></Header>
           <Card>
             <CardHeader title={project.projectName}></CardHeader>
@@ -56,21 +56,63 @@ function DetailView() {
                       "endDate",
                       "budget",
                       "projectManager",
+                      "teamMembers",
                     ].includes(el)
                   ) {
                     return (
                       <ListItem key={el}>
                         <ListItemText
                           primary={`${el.toUpperCase()} : ${project[el]}`}
+                          key={el}
                         />
                       </ListItem>
                     );
                   }
-                })}
 
-                {/* <ListItem>
-                  <ListItemText primary="Spam" />
-                </ListItem> */}
+                  if (el == "tasks") {
+                    return (
+                      <ListItem key={el}>
+                        {project[el].map((task) => {
+                          return (
+                            <ListItemText
+                              primary={<Task task={task} />}
+                              key={el}
+                            />
+                          );
+                        })}
+                      </ListItem>
+                    );
+                  }
+
+                  if (el == "milestones") {
+                    return (
+                      <ListItem key={el}>
+                        {project[el].map((milestone) => {
+                          return (
+                            <ListItemText
+                              primary={
+                                <Milestone milestone={milestone} key={el} />
+                              }
+                            />
+                          );
+                        })}
+                      </ListItem>
+                    );
+                  }
+
+                  if (el == "risks") {
+                    return (
+                      <ListItem key={el}>
+                        {project[el]?.map((risk) => {
+                          return (
+                            <ListItemText primary={<Risk risk={risk} />} />
+                          );
+                        })}
+                      </ListItem>
+                    );
+                  }
+                  return <span style={{ display: "none" }}></span>;
+                })}
               </List>
             </CardContent>
           </Card>
@@ -93,6 +135,77 @@ function Header() {
       </Typography>
     </Toolbar>
   );
+}
+
+function Task(props) {
+  return (
+    <Box>
+      <List>
+        {Object.keys(props.task).map((el) => {
+          if (
+            [
+              "name",
+              "description",
+              "assignedTo",
+              "dueDate",
+              "priority",
+              "status",
+            ].includes(el)
+          ) {
+            return (
+              <ListItem key={el}>
+                <ListItemText
+                  primary={`${el.toUpperCase()} : ${props.task[el]}`}
+                />
+              </ListItem>
+            );
+          }
+
+          return <span style={{ display: "none" }}></span>;
+        })}
+      </List>
+    </Box>
+  );
+}
+
+function Risk(props) {
+  <Box>
+    <List>
+      {Object.keys(props.risk).map((el) => {
+        if (["description", "impact", "mitigationPlan"].includes(el)) {
+          return (
+            <ListItem key={el}>
+              <ListItemText
+                primary={`${el.toUpperCase()} : ${props.risk[el]}`}
+              />
+            </ListItem>
+          );
+        }
+
+        return <span style={{ display: "none" }}></span>;
+      })}
+    </List>
+  </Box>;
+}
+
+function Milestone(props) {
+  <Box>
+    <List>
+      {Object.keys(props.milestone).map((el) => {
+        if (["name", "dueDate", "status"].includes(el)) {
+          return (
+            <ListItem key={el}>
+              <ListItemText
+                primary={`${el.toUpperCase()} : ${props.milestone[el]}`}
+              />
+            </ListItem>
+          );
+        }
+
+        return <span style={{ display: "none" }}></span>;
+      })}
+    </List>
+  </Box>;
 }
 
 export default DetailView;
